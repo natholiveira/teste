67c0106560d108ec7a044d9ebbeb19082612ed25
your name <vo.nathalia@email.com>
2023-04-04 22:29:57 -0300

    subindo o projeto

---
+++ src/main/java/Aula1.java
@@ -0,0 +1,10 @@
+public class Aula1 {
+
+    public static void main(String[] args) {
+        calcular_valores(1.0, 2.0);
+    }
+
+    public static Double calcular_valores(Double valorA, Double valorB) {
+        return valorA + valorB;
+    }
+}
